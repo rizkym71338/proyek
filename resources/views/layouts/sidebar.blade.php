47d0111042ff1ba911a90e3716b42e3f38cf4683
Rizky Maulana <rizkym71338@gmail.com>
2023-07-24 21:07:18 +0700

    update sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -6,96 +6,64 @@
                 <span>Dashboard</span>
             </a>
         </li>
+
+        @can('data-penerimaan')
+            <li class="nav-item">
+                <a class="nav-link {{ request()->is('data-penerimaan') ? '' : 'collapsed' }}"
+                    href="{{ url('/data-penerimaan') }}">
+                    <i class="bi bi-box-arrow-in-down"></i>
+                    <span>Data Penerimaan</span>
+                </a>
+            </li>
+        @endcan
+
+        @can('data-penjualan')
+            <li class="nav-item">
+                <a class="nav-link {{ request()->is('data-penjualan') ? '' : 'collapsed' }}"
+                    href="{{ url('/data-penjualan') }}">
+                    <i class="bi bi-box-arrow-in-up"></i>
+                    <span>Data Penjualan</span>
+                </a>
+            </li>
+        @endcan
+
+        @can('data-persediaan')
+            <li class="nav-item">
+                <a class="nav-link {{ request()->is('data-persediaan') ? '' : 'collapsed' }}"
+                    href="{{ url('/data-persediaan') }}">
+                    <i class="bi bi-sd-card"></i>
+                    <span>Data Persediaan</span>
+                </a>
+            </li>
+        @endcan
+
         @can('laporan-penerimaan')
             <li class="nav-item">
-                <a class="nav-link {{ request()->is('data-penerimaan') || request()->is('laporan-penerimaan') ? '' : 'collapsed' }}"
-                    data-bs-target="#penerimaan" data-bs-toggle="collapse" href="#">
-                    <i class="bi bi-box-arrow-in-down"></i>
-                    <span>Penerimaan</span>
-                    <i class="bi bi-chevron-down ms-auto"></i>
+                <a class="nav-link {{ request()->is('laporan-penerimaan') ? '' : 'collapsed' }}"
+                    href="{{ url('/laporan-penerimaan') }}">
+                    <i class="bi bi-newspaper"></i>
+                    <span>Laporan Penerimaan</span>
                 </a>
-                <ul id="penerimaan"
-                    class="nav-content collapse {{ request()->is('data-penerimaan') || request()->is('laporan-penerimaan') ? 'show' : '' }}"
-                    data-bs-parent="#sidebar-nav">
-                    @can('data-penerimaan')
-                        <li>
-                            <a href="{{ url('/data-penerimaan') }}">
-                                <i class="bi bi-circle"></i>
-                                <span>Data Penerimaan</span>
-                            </a>
-                        </li>
-                    @endcan
-                    @can('laporan-penerimaan')
-                        <li>
-                            <a href="{{ url('/laporan-penerimaan') }}">
-                                <i class="bi bi-circle"></i>
-                                <span>Laporan penerimaan</span>
-                            </a>
-                        </li>
-                    @endcan
-                </ul>
             </li>
         @endcan
 
         @can('laporan-penjualan')
             <li class="nav-item">
-                <a class="nav-link {{ request()->is('data-penjualan') || request()->is('laporan-penjualan') ? '' : 'collapsed' }}"
-                    data-bs-target="#penjualan" data-bs-toggle="collapse" href="#">
-                    <i class="bi bi-box-arrow-in-up"></i>
-                    <span>Penjualan</span>
-                    <i class="bi bi-chevron-down ms-auto"></i>
+                <a class="nav-link {{ request()->is('laporan-penjualan') ? '' : 'collapsed' }}"
+                    href="{{ url('/laporan-penjualan') }}">
+                    <i class="bi bi-newspaper"></i>
+                    <span>Laporan Penjualan</span>
                 </a>
-                <ul id="penjualan"
-                    class="nav-content collapse {{ request()->is('data-penjualan') || request()->is('laporan-penjualan') ? 'show' : '' }}"
-                    data-bs-parent="#sidebar-nav">
-                    @can('data-penjualan')
-                        <li>
-                            <a href="{{ url('/data-penjualan') }}">
-                                <i class="bi bi-circle"></i>
-                                <span>Data Penjualan</span>
-                            </a>
-                        </li>
-                    @endcan
-                    @can('laporan-penjualan')
-                        <li>
-                            <a href="{{ url('/laporan-penjualan') }}">
-                                <i class="bi bi-circle"></i>
-                                <span>Laporan Penjualan</span>
-                            </a>
-                        </li>
-                    @endcan
-                </ul>
             </li>
         @endcan
 
         @can('laporan-persediaan')
             <li class="nav-item">
-                <a class="nav-link {{ request()->is('data-persediaan') || request()->is('laporan-persediaan') ? '' : 'collapsed' }}"
-                    data-bs-target="#persediaan" data-bs-toggle="collapse" href="#">
+                <a class="nav-link {{ request()->is('laporan-persediaan') ? '' : 'collapsed' }}"
+                    href="{{ url('/laporan-persediaan') }}">
                     <i class="bi bi-newspaper"></i>
-                    <span>Persediaan</span>
-                    <i class="bi bi-chevron-down ms-auto"></i>
+                    <span>Laporan Persediaan</span>
                 </a>
-                <ul id="persediaan"
-                    class="nav-content collapse {{ request()->is('data-persediaan') || request()->is('laporan-persediaan') ? 'show' : '' }}"
-                    data-bs-parent="#sidebar-nav">
-                    @can('data-persediaan')
-                        <li>
-                            <a href="{{ url('/data-persediaan') }}">
-                                <i class="bi bi-circle"></i>
-                                <span>Data Persediaan</span>
-                            </a>
-                        </li>
-                    @endcan
-                    @can('laporan-persediaan')
-                        <li>
-                            <a href="{{ url('/laporan-persediaan') }}">
-                                <i class="bi bi-circle"></i>
-                                <span>Laporan Persediaan</span>
-                            </a>
-                        </li>
-                    @endcan
-                </ul>
             </li>
         @endcan
 
